0058747247820f09349ddd3af8d43e2333f72d39
Urban Akos <akosh.urban@gmail.com>
2024-09-29 17:11:07 +0200

    feliratozas

--- 02_rekurzio_kohalmaz_felosztas.py
+++ 02_rekurzio_kohalmaz_felosztas.py
@@ -4,21 +4,22 @@ import os
 
 # Rekurzív függvény a kőhalmaz felosztásához
 def stoneDivision(n, s):
-    # Memoizáció az ismétlődő számítások elkerülésére
+    
+    # az ismétlődő számítások elkerülésére
     memo = {}
 
     def dfs(n):
-        # Ha már kiszámoltuk ezt a halmazt, akkor az eredményt visszaadjuk
+        # ha már kiszámoltuk ezt a halmazt, akkor az eredményt visszaadjuk
         if n in memo:
             return memo[n]
         
         max_moves = 0
         
-        # Iterálunk az összes lehetséges osztón
+        # iterálunk az összes lehetséges osztón
         for x in s:
-            # Csak akkor osztjuk tovább a halmazt, ha az osztható x-szel és az osztás után több, mint 1 részre oszlik
+            # csak akkor osztjuk tovább a halmazt, ha az osztható x-szel és az osztás után több, mint 1 részre oszlik
             if n % x == 0 and n != x:
-                # Az aktuális mozgás számítása: hányszor tudjuk osztani a részeket és hányszor osztható tovább
+                # az aktuális mozgás számítása: hányszor tudjuk osztani a részeket és hányszor osztható tovább
                 parts = n // x
                 max_moves = max(max_moves, parts * dfs(x) + 1)
 
